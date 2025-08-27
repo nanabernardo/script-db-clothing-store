f1bb507b3ab52c949c18664f6bd0c2fafdad80c8
nanabernardo <nanabernardo@gmail.com>
2025-08-27 15:46:09 -0300

    feat: add exclusion and product listing

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import productRoutes from "./products.js";
 import readline from "readline";
 import { exportToExcel } from "./export.js";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { access } from "fs";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -17,25 +20,56 @@ app.use("/products", productRoutes);
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
+//abre conexão com o banco SQLite
+const db = await open({
+  filename: "./store.db",
+  driver: sqlite3.Database,
+});
+
 (async () => {
   let continuar = true;
   const fetch = (await import("node-fetch")).default;
 
   while (continuar) {
-    const name = await question("Digite o nome do produto: ");
-    const quantity = await question("Digite a quantidade em estoque: ");
+    const action = await question(
+      "\nDigite [1] para adicionar, [2] para excluir, [3] para listar produtos ou [0] para sair: "
+    );
 
-    const response = await fetch(`http://localhost:${PORT}/products/add`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, quantity: parseInt(quantity) }),
-    });
+    if (action === "1") {
+      const name = await question("Digite o nome do produto: ");
+      const quantity = await question("Digite a quantidade em estoque: ");
 
-    const data = await response.json();
-    console.log(data);
+      const response = await fetch(`http://localhost:${PORT}/products/add`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, quantity: parseInt(quantity) }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } else if (action === "2") {
+      //excluir produtos
+      const id = await question("Digite o ID do produto a ser excluído: ");
 
-    const again = await question("Deseja cadastrar outro produto? (s/n): ");
-    continuar = again.toLowerCase().startsWith("s");
+      const response = await fetch(`http://localhost:${PORT}/products/${id}`, {
+        method: "DELETE",
+      });
+      console.log(`Produto com ID ${id} excluído.`);
+    } else if (action === "3") {
+      //Listar produtos
+
+      const products = await db.all("SELECT * FROM products");
+
+      console.log("\n--- Produtos em estoque ---");
+      products.forEach((p) => {
+        console.log(
+          `ID: ${p.id} | Nome: ${p.name} | Quantidade: ${p.quantity}`
+        );
+      });
+    } else if (action === "0") {
+      continuar = false; //encerra o loop
+    } else {
+      console.log("Opção inválida");
+    }
   }
 
   //Exporta para Excel
